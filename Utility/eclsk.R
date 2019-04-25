4e838d9e728d20e4a0f50136fccedd63fbfa4b39
JECheadle <j.e.cheadle@utexas.edu>
2019-04-25 07:53:59 -0500

    Added ECLS-K data

---
+++ Utility/eclsk.R
@@ -0,0 +1,5 @@
+
+library(haven)
+eclsk <- read_dta("/Users/jcheadle2/Dropbox/Archive/Teaching/01 UNL/Regression I/2013f/Data/eclsk_k_v02.dta")
+
+save(eclsk, file = "/Users/jcheadle2/Box/Teaching/Current/SOC317L/RSOC317L/Data/eclsk.rda")
